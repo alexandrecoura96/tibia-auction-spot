affc296145bdd774f6c860bee0fb172271102e84
Alexandre <64710438+alexandrecoura96@users.noreply.github.com>
2023-03-04 13:48:42 -0300

    feat: Initial: new screen created

---
+++ src/screens/Initial/index.tsx
@@ -0,0 +1,93 @@
+import { useNavigation } from "@react-navigation/native";
+import React from "react";
+import {
+  Dimensions,
+  Image,
+  ImageBackground,
+  Platform,
+  StatusBar,
+  Text,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Container, Title, OverLayButtomImage } from "./styles";
+import { LinearGradient } from "expo-linear-gradient";
+import { EnterButton } from "../../components/EnterButton";
+
+const backgroundImage = require("../../assets/fankit_offline_female.jpg");
+const overlayBottomImage = require("../../assets/overlay_bottom3.png");
+const tibiaDragonLogo = require("../../assets/tibia_dragon_logo.png");
+const panelSimple = require("../../assets/panel_simple.png");
+const sprite1 = require("../../assets/sprite1.png");
+
+export function Initial() {
+  const navigate = useNavigation();
+  return (
+    <Container
+      source={backgroundImage}
+      resizeMode="cover"
+      style={{ paddingTop: StatusBar.currentHeight }}
+    >
+      <LinearGradient
+        colors={["rgba(0, 0, 0, 0)", "#051122"]}
+        start={{ x: 1, y: 0.1 }}
+        end={{ x: 1, y: 0.6 }}
+        style={{
+          position: "absolute",
+          top: 0,
+          right: 0,
+          bottom: 0,
+          left: 0,
+        }}
+      >
+        <View
+          style={{
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Image
+            source={tibiaDragonLogo}
+            style={{ height: 301 }}
+            resizeMode="center"
+          />
+          <View style={{ paddingLeft: 32, paddingRight: 32, width: "100%" }}>
+            <ImageBackground
+              source={panelSimple}
+              style={{
+                height: 56,
+                width: "100%",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Text
+                style={{ color: "#CBA88E", fontSize: 30, fontWeight: "400" }}
+              >
+                Welcome Tibians
+              </Text>
+            </ImageBackground>
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-evenly",
+
+              width: "100%",
+            }}
+          >
+            <Image source={sprite1} style={{ width: 55.37, height: 61.36 }} />
+            <Image source={sprite1} style={{ width: 55.37, height: 61.36 }} />
+            <Image source={sprite1} style={{ width: 55.37, height: 61.36 }} />
+          </View>
+          <Title style={{ color: "#CBA88E", fontSize: 20, fontWeight: "500" }}>
+            Follow the Bazaar and save time
+          </Title>
+          <EnterButton onPress={() => navigate.navigate("Home")} />
+          <OverLayButtomImage source={overlayBottomImage} resizeMode="cover" />
+        </View>
+      </LinearGradient>
+    </Container>
+  );
+}
---
+++ src/screens/Initial/styles.ts
@@ -0,0 +1,18 @@
+import { Dimensions } from "react-native";
+import styled from "styled-components/native";
+
+export const Container = styled.ImageBackground`
+  flex: 1;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Title = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+export const OverLayButtomImage = styled.Image`
+  width: 100%;
+  height: 105px;
+`;
