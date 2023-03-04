e19c908907ee0be0e930fcddcd9dd72077b5dfba
Alexandre <64710438+alexandrecoura96@users.noreply.github.com>
2023-03-04 13:48:52 -0300

    feat: theme: created theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,36 @@
+export default {
+  colors: {
+    alert: "#F85766",
+    tertiary: "#C98608",
+    background: {
+      primary: "#FAF0D7",
+      secondary: "#F1E0C6",
+      tertiary: "#D4C0A1",
+      quaternary: "#5F4D41",
+    },
+    text_primary: {
+      primary: "#CBA88E",
+      secondary: "#5A2800",
+    },
+    text_complementary: {
+      primary: "#004294",
+      secondary: "#374294",
+    },
+    neutral: "#101010",
+    amber: {
+      primary: "#E2A713",
+      secondary: "#F9D553",
+      tertiary: "#FEFC2E",
+    },
+    indigo: {
+      primary: "#001DD5",
+      secondary: "#0081F2",
+      tertiary: "#0D2CDD",
+    },
+
+    common: {
+      black: "#000000",
+      white: "#FFFFFF",
+    },
+  },
+};
