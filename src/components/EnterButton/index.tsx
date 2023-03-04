482a0c8bad6ee8f6821dccb0e62987b076b6c92c
Alexandre <64710438+alexandrecoura96@users.noreply.github.com>
2023-03-04 14:08:18 -0300

    feat: EnterButton: refinements

--- src/components/EnterButton/index.tsx
+++ src/components/EnterButton/index.tsx
@@ -8,18 +8,26 @@ interface IEnterButtonProps extends TouchableOpacityProps {}
 
 export function EnterButton({ ...props }: IEnterButtonProps) {
   return (
-    <Container activeOpacity={0.8} {...props}>
+    <Container
+      activeOpacity={0.8}
+      {...props}
+      style={{
+        borderRadius: 4,
+        borderWidth: 4,
+        borderTopColor: "#FEFC2E",
+        borderLeftColor: "#E2A713",
+        borderBottomColor: "#C98608",
+        borderRightColor: "#E2A713",
+      }}
+    >
       <LinearGradient
         colors={["#001DD5", "#0081F2", "#0D2CDD"]}
         start={{ x: 1, y: 0.9 }}
         end={{ x: 1, y: 0.1 }}
         style={{
           borderRadius: 4,
-          borderWidth: 4,
-          borderTopColor: "#FEFC2E",
-          borderLeftColor: "#E2A713",
-          borderBottomColor: "#C98608",
-          borderRightColor: "#E2A713",
+          borderWidth: 2,
+          borderColor: "#000",
           height: 52,
           width: 240,
           alignItems: "center",
