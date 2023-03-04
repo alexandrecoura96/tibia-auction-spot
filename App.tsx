733c8a2019622b305b4f2088f79bfd22ed79c8b5
Alexandre <64710438+alexandrecoura96@users.noreply.github.com>
2023-03-04 13:49:09 -0300

    feat: App: modifications implementeds

--- App.tsx
+++ App.tsx
@@ -1,7 +1,54 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { Home } from "./src/screens/Home";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { AppStackRoutes } from "./src/routes/app.stack.routes";
+import { StatusBar, Platform } from "react-native";
+import {
+  Martel_300Light,
+  Martel_400Regular,
+  Martel_600SemiBold,
+  Martel_700Bold,
+  Martel_800ExtraBold,
+  Martel_900Black,
+  useFonts,
+} from "@expo-google-fonts/martel";
+import {
+  Livvic_400Regular,
+  Livvic_500Medium,
+  Livvic_600SemiBold,
+  Livvic_700Bold,
+  Livvic_900Black,
+} from "@expo-google-fonts/livvic";
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
-  return <Home />;
+  const [fontsLoaded] = useFonts({
+    Martel_300Light,
+    Martel_400Regular,
+    Martel_600SemiBold,
+    Martel_700Bold,
+    Martel_800ExtraBold,
+    Martel_900Black,
+    Livvic_400Regular,
+    Livvic_500Medium,
+    Livvic_600SemiBold,
+    Livvic_700Bold,
+    Livvic_900Black,
+  });
+
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <StatusBar
+          barStyle={Platform.OS === "ios" ? "dark-content" : "default"}
+          translucent
+          backgroundColor="transparent"
+        />
+        <AppStackRoutes />
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
 }
