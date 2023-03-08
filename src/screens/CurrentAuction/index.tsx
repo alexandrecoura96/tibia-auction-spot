902e9a336f5b5a30feb79804ba6600519b9c45bd
Alexandre <64710438+alexandrecoura96@users.noreply.github.com>
2023-03-08 00:10:14 -0300

    chore: CurrentAuction: refinements

--- src/screens/CurrentAuction/index.tsx
+++ src/screens/CurrentAuction/index.tsx
@@ -10,6 +10,7 @@ import {
   ActivityIndicator,
 } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
+import { CharacterCard } from "../../components/CharacterCard";
 import { api } from "../../libs/axios";
 import { DataType } from "./types";
 
@@ -40,59 +41,84 @@ export function CurrentAuction() {
 
   if (loading) {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <ActivityIndicator size={32} color="#00CB00" />
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "#FFF0D9",
+        }}
+      >
+        <ActivityIndicator size={32} color="#5A2800" />
       </View>
     );
   }
 
   return (
-    <FlatList
-      data={data}
-      keyExtractor={(item, index) => `${item.name} + ${index}`}
-      contentContainerStyle={{ padding: 16 }}
-      renderItem={({ item }) => {
-        return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <FlatList
+        data={data}
+        keyExtractor={(item, index) => `${item.name} + ${index}`}
+        ListHeaderComponent={
           <View
             style={{
-              borderWidth: 0.5,
-              borderColor: "#f1e0c6",
-              marginTop: 8,
-              borderRadius: 8,
-              padding: 16,
-              backgroundColor: "#f1e0c6",
+              height: 48,
+              width: "100%",
+              backgroundColor: "#5F4D41",
+              marginTop: StatusBar.currentHeight,
             }}
-          >
-            <View
-              style={{
-                flexDirection: "row",
-                alignItems: "center",
-                justifyContent: "space-between",
-                flex: 1,
-              }}
-            >
-              <Image
-                source={{ uri: item.outfitUrl }}
-                style={{
-                  height: 100,
-                  width: 100,
-                }}
-              />
-              <View>
-                <Text>Name: {item.name}</Text>
-                <Text>Vocation: {item.vocation}</Text>
-                <Text>Level: {item.level}</Text>
-                <Text>World: {item.world}</Text>
-              </View>
+          />
+        }
+        contentContainerStyle={{ backgroundColor: "#FFF0D9" }}
+        ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
+        renderItem={({ item }) => {
+          return (
+            <View style={{ padding: 16 }}>
+              <CharacterCard {...item} />
             </View>
-            <View style={{ marginTop: 24 }}>
-              <Text>Current Bid: {item.bid}</Text>
-              <Text>Auction Start: {item.auctionStart}</Text>
-              <Text>Auction End: {item.auctionEnd}</Text>
-            </View>
-          </View>
-        );
-      }}
-    />
+
+            // <View
+            //   style={{
+            //     borderWidth: 0.5,
+            //     borderColor: "#f1e0c6",
+            //     marginTop: 8,
+            //     borderRadius: 8,
+            //     padding: 16,
+            //     backgroundColor: "#f1e0c6",
+            //   }}
+            // >
+            //   <View
+            //     style={{
+            //       flexDirection: "row",
+            //       alignItems: "center",
+            //       justifyContent: "space-between",
+            //       flex: 1,
+            //     }}
+            //   >
+            //     <Image
+            //       source={{ uri: item.outfitUrl }}
+            //       style={{
+            //         height: 100,
+            //         width: 100,
+            //       }}
+            //     />
+            //     <View>
+            //       <Text>Name: {item.name}</Text>
+            //       <Text>Vocation: {item.vocation}</Text>
+            //       <Text>Level: {item.level}</Text>
+            //       <Text>World: {item.world}</Text>
+            //     </View>
+            //   </View>
+            //   <View style={{ marginTop: 24 }}>
+            //     <Text>Current Bid: {item.bid}</Text>
+            //     <Text>Auction Start: {item.auctionStart}</Text>
+            //     <Text>Auction End: {item.auctionEnd}</Text>
+            //   </View>
+            // </View>
+          );
+        }}
+      />
+    </>
   );
 }
