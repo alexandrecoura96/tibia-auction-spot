af0ad0cd35a9db1c59d58639097d50ad76886d93
Alexandre <64710438+alexandrecoura96@users.noreply.github.com>
2023-03-09 09:51:45 -0300

    chore: countdown: refinements

---
+++ src/utils/countdown.ts
@@ -0,0 +1,46 @@
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+export function getTimeLeft(auctionEnd: number) {
+  const now = dayjs().utc();
+  const end = dayjs.unix(auctionEnd).utc();
+  const endCET = end;
+  // console.log(endCET);
+
+  const duration = dayjs.duration(end.diff(now));
+
+  if (duration.asMilliseconds() <= 0) {
+    return "Auction Ended";
+  }
+
+  const days = duration.days();
+  const hours = duration.hours();
+  const minutes = duration.minutes();
+  const seconds = duration.seconds();
+
+  if (days > 0) {
+    return `${days}d ${hours}h ${minutes}m ${seconds}s, ${end
+      .add(1, "hour")
+      .tz("Europe/Paris")
+      .format("HH:mm")}`;
+  } else if (hours > 0) {
+    return `${hours}h ${minutes}m ${seconds}s, ${end
+      .add(1, "hour")
+      .tz("Europe/Paris")
+      .format("HH:mm CET")}`;
+  } else if (minutes > 0) {
+    return `${minutes}m ${seconds}s, ${end
+      .add(1, "hour")
+      .tz("Europe/Paris")
+      .format("HH:mm CET")}`;
+  } else {
+    return `${seconds}s, ${end
+      .add(1, "hour")
+      .tz("Europe/Paris")
+      .format("HH:mm CET")}`;
+  }
+}
