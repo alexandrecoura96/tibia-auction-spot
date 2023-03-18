be99a020aa00f4b4e8e8d4650578aff62fc7ba4b
Alexandre <64710438+alexandrecoura96@users.noreply.github.com>
2023-03-18 12:27:47 -0300

    feat: CharacterDetail: created style

--- src/screens/CharacterDetails/index.tsx
+++ src/screens/CharacterDetails/index.tsx
@@ -1,7 +1,15 @@
-import { Route, useRoute } from "@react-navigation/native";
-import React from "react";
-import { View, Text, StatusBar } from "react-native";
+import { Route, useNavigation, useRoute } from "@react-navigation/native";
+import { FlashList } from "@shopify/flash-list";
+import React, { useEffect, useState } from "react";
+import { View, Text, StatusBar, Alert } from "react-native";
+import { BackButton } from "../../components/BackButton";
+import { CharacterInsider } from "../../components/CharacterInsider";
+import { GeneralDetails } from "../../components/GeneralDetails";
+import { Loading } from "../../components/Loading";
+import { LoadingScreen } from "../../components/LoadingScreen";
+import { api } from "../../libs/axios";
 import { DataType } from "../CurrentAuction/types";
+import { Container, HeaderTitle, HeaderWrapper } from "./styles";
 
 type Params = {
   item: DataType;
@@ -10,10 +18,76 @@ type Params = {
 type MyRoute = Route<"CurrentAuction", Params>;
 
 export function CharacterDetails() {
+  const { goBack } = useNavigation();
   const { params } = useRoute<MyRoute>();
+
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  async function fetchCharacterDetailsList() {
+    try {
+      setLoading(true);
+      const response = await api.get("/bazar/character-details", {
+        params: { auctionId: 1179843 },
+      });
+
+      setData(response.data);
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Ops", "Não possível carregar as informações");
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchCharacterDetailsList();
+  }, []);
+
+  if (loading) {
+    return <LoadingScreen />;
+  }
+
   return (
-    <View style={{ marginTop: StatusBar.currentHeight }}>
-      <Text>{params.item.name}</Text>
-    </View>
+    <Container style={{ flex: 1 }}>
+      <StatusBar barStyle="dark-content" />
+      <FlashList
+        keyExtractor={(item, index) => `${item.label} + ${index}`}
+        estimatedItemSize={40}
+        data={data}
+        ListHeaderComponentStyle={{
+          marginTop: StatusBar.currentHeight,
+        }}
+        ListHeaderComponent={
+          <View style={{ paddingHorizontal: 24 }}>
+            <HeaderWrapper>
+              <BackButton onPress={goBack} />
+              <HeaderTitle>Details Account</HeaderTitle>
+            </HeaderWrapper>
+            <CharacterInsider {...params.item} />
+          </View>
+        }
+        contentContainerStyle={{
+          paddingBottom: 80,
+        }}
+        renderItem={({ item, index }) => (
+          <View key={index} style={{ paddingHorizontal: 24 }}>
+            <GeneralDetails {...item} />
+          </View>
+        )}
+      />
+    </Container>
   );
 }
+// <View style={{ paddingTop: StatusBar.currentHeight }}>
+//   <StatusBar barStyle="dark-content" />
+//   <HeaderWrapper>
+//     <BackButton onPress={goBack} />
+//     <HeaderTitle>Details Account</HeaderTitle>
+//   </HeaderWrapper>
+//   <Text>{params.item.name}</Text>
+//   {data.map((item, index) => (
+//     <View key={index}>
+//       <GeneralDetails {...item} />
+//     </View>
+//   ))}
---
+++ src/screens/CharacterDetails/styles.ts
@@ -0,0 +1,22 @@
+import { RFValue } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.common.neutral};
+`;
+
+export const HeaderWrapper = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const HeaderTitle = styled.Text`
+  font-size: ${RFValue(30)}px;
+  line-height: ${RFValue(50)}px;
+  color: ${({ theme }) => theme.colors.text_primary.secondary};
+  font-family: ${({ theme }) => theme.fonts.martel_400};
+  flex: 1;
+  text-align: center;
+`;
