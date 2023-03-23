e00c799918f3a00038bbd1b077b504042a974499
Alexandre <64710438+alexandrecoura96@users.noreply.github.com>
2023-03-23 11:38:46 -0300

    chore: EnterButton: refinements

--- src/components/EnterButton/index.tsx
+++ src/components/EnterButton/index.tsx
@@ -1,42 +1,20 @@
-import { LinearGradient } from "expo-linear-gradient";
 import React from "react";
 import { TouchableOpacityProps } from "react-native";
-import { RFValue } from "react-native-responsive-fontsize";
 import { LinearGradientTitle } from "../LinearGradientTitle";
-import { Container } from "./styles";
+import { Container, Linear } from "./styles";
 
 interface IEnterButtonProps extends TouchableOpacityProps {}
 
 export function EnterButton({ ...props }: IEnterButtonProps) {
   return (
-    <Container
-      activeOpacity={0.8}
-      {...props}
-      style={{
-        borderRadius: 4,
-        borderWidth: 4,
-        borderTopColor: "#FEFC2E",
-        borderLeftColor: "#E2A713",
-        borderBottomColor: "#C98608",
-        borderRightColor: "#E2A713",
-      }}
-    >
-      <LinearGradient
+    <Container activeOpacity={0.8} {...props}>
+      <Linear
         colors={["#001DD5", "#0081F2", "#0D2CDD"]}
         start={{ x: 1, y: 0.9 }}
         end={{ x: 1, y: 0.1 }}
-        style={{
-          borderRadius: 4,
-          borderWidth: 2,
-          borderColor: "#000",
-          height: RFValue(52),
-          width: RFValue(240),
-          alignItems: "center",
-          justifyContent: "center",
-        }}
       >
         <LinearGradientTitle title="To Start" />
-      </LinearGradient>
+      </Linear>
     </Container>
   );
 }
--- src/components/EnterButton/styles.ts
+++ src/components/EnterButton/styles.ts
@@ -1,10 +1,27 @@
+import { LinearGradient } from "expo-linear-gradient";
 import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
 export const ButtonName = styled.Text`
-  color: #f9d553;
+  color: ${({ theme }) => theme.colors.amber.secondary};
   font-size: ${RFValue(24)}px;
   font-weight: bold;
   font-family: ${({ theme }) => theme.fonts.martel_900};
 `;
-export const Container = styled.TouchableOpacity``;
+export const Container = styled.TouchableOpacity`
+  border-radius: ${RFValue(4)}px;
+  border-width: ${RFValue(4)}px;
+  border-top-color: ${({ theme }) => theme.colors.amber.tertiary};
+  border-left-color: ${({ theme }) => theme.colors.amber.primary};
+  border-bottom-color: ${({ theme }) => theme.colors.amber.quaternary};
+  border-right-color: ${({ theme }) => theme.colors.amber.primary};
+`;
+
+export const Linear = styled(LinearGradient)`
+  border: ${RFValue(2)}px solid #000;
+  border-radius: ${RFValue(4)}px;
+  height: ${RFValue(52)}px;
+  width: ${RFValue(240)}px;
+  align-items: center;
+  justify-content: center;
+`;
