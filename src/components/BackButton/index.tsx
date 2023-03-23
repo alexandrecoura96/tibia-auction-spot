8771ad6535ca71656a8a2553dac47e87931cccba
Alexandre <64710438+alexandrecoura96@users.noreply.github.com>
2023-03-23 11:38:31 -0300

    chore: BackButton: refinements

--- src/components/BackButton/index.tsx
+++ src/components/BackButton/index.tsx
@@ -2,11 +2,17 @@ import React from "react";
 import Feather from "@expo/vector-icons/Feather";
 import { Container } from "./styles";
 import { IBackButtonProps } from "./types";
+import { useTheme } from "styled-components/native";
 
 export function BackButton({ ...props }: IBackButtonProps) {
+  const theme = useTheme();
   return (
     <Container {...props}>
-      <Feather name="arrow-left" size={32} color="#5A2800" />
+      <Feather
+        name="arrow-left"
+        size={32}
+        color={theme.colors.text_primary.secondary}
+      />
     </Container>
   );
 }
