66b528dc070139bfc43f53b2fefb35734c2e9e08
Alexandre <64710438+alexandrecoura96@users.noreply.github.com>
2023-03-23 11:38:58 -0300

    chore: LoadingScreen: refinements

--- src/components/LoadingScreen/index.tsx
+++ src/components/LoadingScreen/index.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { ActivityIndicator, StatusBar } from "react-native";
+import { useTheme } from "styled-components/native";
 import { Container } from "./styles";
 
 export function LoadingScreen() {
+  const theme = useTheme();
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <Container>
-        <ActivityIndicator size={32} color="#5A2800" />
+        <ActivityIndicator
+          size={32}
+          color={theme.colors.text_primary.secondary}
+        />
       </Container>
     </>
   );
