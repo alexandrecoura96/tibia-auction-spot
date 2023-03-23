919ce3b70fb46ae49117e6375621f0b29fa130ac
Alexandre <64710438+alexandrecoura96@users.noreply.github.com>
2023-03-23 11:38:52 -0300

    chore: Loading: refinements

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { ActivityIndicator } from "react-native";
+import { useTheme } from "styled-components/native";
 import { Container } from "./styles";
 
 export function Loading() {
+  const theme = useTheme();
   return (
     <Container>
-      <ActivityIndicator color="#5A2800" />
+      <ActivityIndicator color={theme.colors.text_primary.secondary} />
     </Container>
   );
 }
