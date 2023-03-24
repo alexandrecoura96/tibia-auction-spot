4e3b46ed849cd32f68d74b4c6842db0d7b2d3dd6
Alexandre <64710438+alexandrecoura96@users.noreply.github.com>
2023-03-23 22:26:37 -0300

    chore: Header: refinements

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { FilterButton } from "../FilterButton";
 import {
   Background,
   Container,
+  FilterName,
+  FilterWrapper,
   HeaderTitle,
   ResultDescription,
 } from "./styles";
@@ -9,12 +12,21 @@ import { HeaderProps } from "./types";
 
 const header_background = require("../../assets/header_background.png");
 
-export function Header({ title, resultDescription }: HeaderProps) {
+export function Header({
+  title,
+  resultDescription,
+  onFilterPress,
+}: HeaderProps) {
   return (
     <Container>
       <Background source={header_background}>
         <HeaderTitle>{title}</HeaderTitle>
       </Background>
+      <FilterWrapper>
+        <FilterName>Filters</FilterName>
+        <FilterButton title="World" onPress={onFilterPress} />
+        <FilterButton title="Vocation" />
+      </FilterWrapper>
       <ResultDescription>{resultDescription}</ResultDescription>
     </Container>
   );
