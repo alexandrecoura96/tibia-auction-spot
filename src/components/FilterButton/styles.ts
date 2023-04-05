213b84f869c30fec7edac295b3ac59175d670bbe
Alexandre <64710438+alexandrecoura96@users.noreply.github.com>
2023-04-05 15:37:37 -0300

    chore: FilterButton: refinements

--- src/components/FilterButton/styles.ts
+++ src/components/FilterButton/styles.ts
@@ -2,13 +2,13 @@ import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
 export const Container = styled.TouchableOpacity`
-  border: 1px solid #5f4d41;
+  border: 1px solid ${({ theme }) => theme.colors.background.quaternary};
   padding: ${RFValue(8)}px ${RFValue(16)}px;
   margin-left: ${RFValue(24)}px;
   margin-top: ${RFValue(8)}px;
   align-items: center;
   justify-content: center;
-  background-color: #d4c0a1;
+  background-color: ${({ theme }) => theme.colors.background.tertiary};
 `;
 export const Title = styled.Text`
   font-size: ${RFValue(16)}px;
