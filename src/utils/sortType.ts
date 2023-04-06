65bc7b0e9ab8413b405b6687d99c52e5d1a4fe21
Alexandre <64710438+alexandrecoura96@users.noreply.github.com>
2023-04-06 16:56:35 -0300

    feat: created sortType & vocation

---
+++ src/utils/sortType.ts
@@ -0,0 +1,45 @@
+interface SortTypeProps {
+  id: string;
+  order_column: string;
+  order_direction: string;
+  name: string;
+}
+
+export const SortTypeNames: SortTypeProps[] = [
+  {
+    id: "1",
+    order_column: "102",
+    order_direction: "0",
+    name: "highest level",
+  },
+  {
+    id: "2",
+    order_column: "102",
+    order_direction: "1",
+    name: "lowest  level",
+  },
+  {
+    id: "3",
+    order_column: "101",
+    order_direction: "0",
+    name: "latest on bazar",
+  },
+  {
+    id: "4",
+    order_column: "101",
+    order_direction: "1",
+    name: "earliest on bazar",
+  },
+  {
+    id: "5",
+    order_column: "100",
+    order_direction: "0",
+    name: "highest bid",
+  },
+  {
+    id: "6",
+    order_column: "100",
+    order_direction: "1",
+    name: "lowest bid",
+  },
+];
---
+++ src/utils/vocations.ts
@@ -0,0 +1,13 @@
+interface VocationsProps {
+  id: string;
+  name: string;
+}
+
+export const VocationsNames: VocationsProps[] = [
+  { id: "-1", name: "All" },
+  { id: "1", name: "None" },
+  { id: "2", name: "Druid" },
+  { id: "3", name: "Knight" },
+  { id: "4", name: "Paladin" },
+  { id: "5", name: "Sorcerer" },
+];
