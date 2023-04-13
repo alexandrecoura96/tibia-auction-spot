2cbe2563f6d5fa9fb2abd2f406f65590ce423504
Alexandre <64710438+alexandrecoura96@users.noreply.github.com>
2023-04-13 18:04:50 -0300

    chore: filter refinements

--- src/screens/CurrentAuction/Controller.tsx
+++ src/screens/CurrentAuction/Controller.tsx
@@ -50,18 +50,14 @@ export const Controller = () => {
       if (newData.length === 0) {
         setAllDataLoaded(true);
       } else {
+        setData(newData);
+        setLastOrderColumn(order_column);
+        setLastOrderDirection(order_direction);
         if (worldName !== lastWorldName) {
           setData(newData);
           setLastWorldName(worldName);
         }
-        if (order_column !== lastOrderColumn) {
-          setData(newData);
-          setLastOrderColumn(order_column);
-        }
-        if (order_direction !== lastOrderDirection) {
-          setData(newData);
-          setLastOrderDirection(order_direction);
-        }
+
         if (vocation !== lastVocationName) {
           setData(newData);
           setLastVocationName(vocation);
